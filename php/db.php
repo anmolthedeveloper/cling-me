40adbc15d0986365af7326aa2276ed5a5a59df7b
Peter <coderscave1@gmail.com>
2018-05-16 13:05:42 +0300

    for database connectivity

--- php/db.php
+++ php/db.php
@@ -1,7 +1,7 @@
 <?php
     $host = 'localhost';
-    $usr = 'aa.33Alpp-32';
-    $pass = '8YH9YBgBTFVUT';
+    $usr = 'root';
+    $pass = '';
     $database = 'ClientDatabases-33353a30';
     $db = new mysqli($host,$usr,$pass,$database);
 ?>
